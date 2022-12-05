f1b9b58ab67317a4ed2de50f1dc3f71f492582f3
Janez Justin <janez.justin0@gmail.com>
2022-12-05 22:04:50 +0100

    Day 5 ⭐️

---
+++ 2022/05/1.py
@@ -0,0 +1,32 @@
+from input import input
+
+stacks = {}
+init_phase = True
+
+for line in input.split('\n'):
+    if init_phase:
+        if line == "":
+            print(stacks)
+            init_phase = False
+            continue
+        crates = [line[i+1:i+2] for i in range(0, len(line), 4)]
+        for (i, crate) in enumerate(crates, 1):
+            if crate == " ":
+                continue
+            if i not in stacks:
+                stacks[i] = ""
+            stacks[i] += crate
+        continue
+
+    [_, num, _, fr, _, to] = line.split(" ")
+    num, fr, to = int(num), int(fr), int(to)
+
+    for i in range(num):
+        move = stacks[fr][0]
+        stacks[fr] = stacks[fr][1:]
+        stacks[to] = move + stacks[to]
+
+x = ""
+for i in range(1, len(stacks)+1):
+    x += stacks[i][0]
+print(x)
