50ead8b2abd08628381a61a52cd0192199fa718a
Janez Justin <janez.justin0@gmail.com>
2022-12-06 22:16:08 +0100

    Day 6 ⭐️

---
+++ 2022/06/2.py
@@ -0,0 +1,10 @@
+from input import input
+
+l = 14
+last4chars = ["-"] * l
+for (i, char) in enumerate(input):
+    if len(set(last4chars)) != l or "-" in last4chars:
+        last4chars = last4chars[1:l] + [char]
+    else:
+        print(i)
+        break
