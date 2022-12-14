13dd04f63c072f8c11a93ed2729eede68f13e63f
Janez Justin <janez.justin0@gmail.com>
2022-12-14 07:37:37 +0100

    Day 13 ⭐️

---
+++ 2022/13/2.py
@@ -0,0 +1,75 @@
+import itertools as it
+import os
+import subprocess
+import functools as ft
+import numpy as np
+import parse
+try:
+    from utils import *
+except ImportError:
+    print("No utils found, not importing.")
+
+
+def solve(input: str):
+    res = "no result provided"
+    res = 1
+    input = input.strip()
+    input = input.split('\n\n')
+    vals = [[[2]], [[6]]]
+    for i, pair in enumerate(input):
+        [p1, p2] = pair.split('\n')
+        p1 = eval(p1)
+        p2 = eval(p2)
+
+        vals += [p1, p2]
+
+    vals.sort(key=ft.cmp_to_key(sort_compare))
+    for i, pair in enumerate(vals):
+        if pair in [[[2]], [[6]]]:
+            res *= (i + 1)
+    return res
+
+
+def sort_compare(p1, p2):
+    if compare(p1, p2)[0]:
+        return -1
+    return 1
+
+
+def compare(p1, p2):
+    if p1 == p2:
+        return True, True
+    if isinstance(p1, int) and isinstance(p2, int):
+        return p1 < p2, False
+    if isinstance(p1, int) and isinstance(p2, list):
+        p1 = [p1]
+        return compare(p1, p2)
+    if isinstance(p1, list) and isinstance(p2, int):
+        p2 = [p2]
+        return compare(p1, p2)
+
+    for i in range(max(len(p1), len(p2))):
+        if i >= len(p1):
+            return True, False
+        if i >= len(p2):
+            return False, False
+        t, cont = compare(p1[i], p2[i])
+        if not cont:
+            return t, False
+        if not t:
+            return False
+
+    return True, True
+
+
+with open(f"{os.path.dirname(__file__)}/input", "r") as f:
+    input = f.read()
+
+
+def handle_result(result):
+    subprocess.run("pbcopy", text=True, input=result)
+    print(result)
+
+
+out = solve(input)
+handle_result(str(out))
