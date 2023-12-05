4cd0f89c54f9506ab5f113f77d48b1bc8162393c
Janez Justin <janez.justin0@gmail.com>
2023-12-05 23:52:05 +0100

    2020: Day 1 ⭐⭐

---
+++ 2020/01/2.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import functools as ft
+import itertools as it
+import os
+import subprocess
+from collections import defaultdict
+
+import numpy as np
+
+from parse import *
+
+try:
+    from utils import *
+except ImportError:
+    print("No utils found, not importing.")
+
+
+def solve(input: str):
+    input = input.strip()
+    res = 0
+
+    data = []
+    for line in input.split("\n"):
+        if line == "":
+            continue
+
+        data.append(int(line))
+    print(data)
+
+    for i in data:
+        for j in data:
+            for k in data:
+                if j + k + i == 2020:
+                    return i * j * k
+    return res
+
+
+def handle_result(result):
+    subprocess.run("pbcopy", text=True, input=result)
+    print(result)
+
+
+with open(f"{os.path.dirname(__file__)}/input", "r") as f:
+    input = f.read()
+
+
+out = solve(input)
+handle_result(str(out))
