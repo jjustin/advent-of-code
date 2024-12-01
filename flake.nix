898fedf63d52cad110c88a98037c82028b036ce0
Janez Justin <janez.justin0@gmail.com>
2024-12-01 11:24:10 +0100

    nix setup

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python3
+            (python3.withPackages (python-pkgs: with python-pkgs; [
+              numpy
+              parse
+            ]))
+          ];
+        };
+      }
+    );
+}
